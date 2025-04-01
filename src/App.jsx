f6d8788032732d647ada54dd226e9ddd3af34528
Mikita Stankevich <stankevich.mikita21@gmail.com>
2025-04-01 15:46:00 +0200

    That's actually it

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,64 @@ function App() {
       .then((json) => {
         console.log("json", json);
         setComments(json);
-        console.log("comments", comments);
       });
   }, []);
 
-  useEffect(() => {
-    console.log("Updated comments:", comments);
-  }, [comments]);
+  const [scrollTop, setScrollTop] = useState(0);
+  const itemHeight = 35;
+  const windowHeight = 300;
+  const overscan = 5;
+  const startIndex = Math.max(0, Math.floor(scrollTop / itemHeight) - overscan);
+  const endIndex = Math.min(
+    Math.floor((scrollTop + windowHeight) / itemHeight) + overscan,
+    comments.length - 1
+  );
+
+  const visibleComments = comments.slice(startIndex, endIndex + 1);
+
+  const generateRows = (comments) => {
+    return comments.map((item, index) => (
+      <ListItem
+        key={item.id}
+        index={startIndex + index}
+        item={item}
+        itemHeight={itemHeight}
+      />
+    ));
+  };
 
   return (
     <div>
-      <div className="container">
-        {comments.map((el) => (
-          <div className="element" key={el.id}>
-            <span>{el.email}</span>
+      <div
+        className="container"
+        style={{ height: `${windowHeight}px`, overflowY: "auto" }}
+        onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
+      >
+        <div style={{ height: `${comments.length * itemHeight}px` }}>
+          <div
+            style={{ transform: `translateY(${startIndex * itemHeight}px)` }}
+          >
+            {generateRows(visibleComments)}{" "}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
 }
+
+const ListItem = ({ index, itemHeight, item }) => {
+  return (
+    <div
+      style={{
+        height: `${itemHeight}px`,
+      }}
+      className="element"
+    >
+      <span>
+        {item.email} - {`index: ${index}`}
+      </span>
+    </div>
+  );
+};
 
 export default App;
